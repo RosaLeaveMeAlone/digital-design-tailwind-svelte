132e017345a51330bf7630f09094f8f08259fa1a
Joel Uzcategui <joel@tianguis-mx.com>
2023-11-26 10:59:40 -0400

    feature, creations and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Josefin Sans', 'sans-serif'],
+        alata: ['Alata', 'sans-serif'],
+      },
+      letterSpacing: {
+        widest: '.3em',
+      }
+    },
   },
   plugins: [],
 }
